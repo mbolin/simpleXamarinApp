116317575de0207b17de219dd0b3de493cd3adc4
Matthew Bolin <Matthew.Bolin@autozone.com>
2021-08-24 10:14:48 -0400

    simple xamarin app

---
+++ SimpleXamarinApp/SimpleXamarinApp/Model/People.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SimpleXamarinApp.Model
+{
+    public class People
+    {
+        public ObservableCollection<Account> Persons { get; }
+
+        public People()
+        {
+            Persons = new ObservableCollection<Account>();
+        }
+    }
+
+    public class Account
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ SimpleXamarinApp/SimpleXamarinApp/ViewModel/MyModelViewModel.cs
@@ -0,0 +1,94 @@
+﻿using SimpleXamarinApp.Model;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SimpleXamarinApp.ViewModel
+{
+    public class MyModelViewModel : BindableObject
+    {
+        public ICommand AddCommand { get; }
+        public ICommand RemoveCommand { get; }
+        public ICommand ConstantAddCommand { get; }
+
+        public People People { get; }
+
+        Thread t;
+
+        bool isRunning = false;
+
+        string currentName;
+        public string CurrentName
+        {
+            get => currentName;
+            set
+            {
+                if (currentName == value)
+                    return;
+                currentName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        Account selectedPerson;
+        public Account SelectedPerson
+        {
+            get => selectedPerson;
+            set
+            {
+                if (selectedPerson == value)
+                    return;
+                selectedPerson = value;
+                CurrentName = value.Name;
+                OnPropertyChanged();
+            }
+        }
+
+        public MyModelViewModel()
+        {
+            AddCommand = new Command(AddName);
+            RemoveCommand = new Command(RemoveName);
+            ConstantAddCommand = new Command(ConstantAddName);
+            People = new People();
+            People.Persons.Add(new Account() { Name = "bob" });
+            People.Persons.Add(new Account() { Name = "steve" });
+        }
+
+        void AddName()
+        {
+            People.Persons.Add(new Account() { Name = CurrentName });
+        }
+
+        void RemoveName()
+        {
+            People.Persons.Remove(SelectedPerson);
+        }
+
+        void ConstantAddName()
+        {
+            if (isRunning)
+            {
+                t.Abort();
+                isRunning = false;
+            }
+            else
+            {
+                t = new Thread(threadedAdding);
+                t.Start();
+                isRunning = true;
+            }
+        }
+
+        void threadedAdding()
+        {
+            int count = 0;
+            while (true)
+            {
+                People.Persons.Add(new Account() { Name = "jack" + count++ });
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
